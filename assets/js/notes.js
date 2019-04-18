8531f8d9aa3c3f0b5ac9289ea8ae4c2d83f3f98b
nengak <nengakdakup@gmail.com>
2019-04-18 18:01:57 -0400

    hehehehe, more and more javascript work

--- assets/js/notes.js
+++ assets/js/notes.js
@@ -1,18 +1,15 @@
-
-
 //to get the user click on the button
-document.getElementById('addItem').addEventListener("click", function(){
-  var value = document.getElementById('item').value;
+document.querySelector('.create_note_btn').addEventListener("click", function(){
+  var value = document.querySelector('.create_note_input').value;
   if (value) {
-    addItemTodo(value);
-    document.getElementById('item').value = '';
-
+    addNote(value);
     data.todo.push(value);
   }
   dataObjectUpdated();
+  console.log(value);
 });
 
-document.getElementById('item').addEventListener('keydown', function(e){
+document.querySelector('.create_note_input').addEventListener('keydown', function(e){
   var value = this.value;
   if (e.code === 'Enter' && value) {
     addItemTodo(value);
