70070b6c7a01f6b210bf552ca5910f2ca9bc4375
nengak <nengakdakup@gmail.com>
2019-04-19 05:00:45 -0400

    pheew, lot of work done, notes can now be rendered to main activity page

--- assets/js/notes.js
+++ assets/js/notes.js
@@ -7,7 +7,7 @@ var notes = {
 document.querySelector('.create_note_btn').addEventListener("click", function(){
   var value = document.querySelector('.create_note_input').value;
   if (value) {
-    addNote(value);
+    displayEditor(value);
     document.querySelector('.create_note_input').value = "";
   }
 });
@@ -21,6 +21,7 @@ document.querySelector('.create_note_input').addEventListener('keydown', functio
   }
 });
 
+//display or hide the main editor
 function displayEditor(value) {
   if (value) {
     document.querySelector('.note_editor').style.display = "unset";
@@ -39,5 +40,46 @@ document.querySelector('.save_note_btn').addEventListener("click", function(){
   var title = document.querySelector('.create_note_input_main').value;
   if (content && title) {
     displayEditor();
+    renderNotes(title);
+    notes.title.push(title);
+    notes.title_content.push(content);
   }
 });
+
+document.querySelector('.discard_note_btn').addEventListener("click", function(){
+  displayEditor();
+  document.querySelector('#create_note_toggle');
+});
+
+function renderNotes(title) {
+  //creating the icons
+  var editBtnicon = '<i class="fa fa-edit"></i>'; //declare the edit btn inner icon
+  var deleteBtnicon = '<i class="fa fa-trash-alt"></i>'; //declare the idelete btn inner icon
+
+  var listContainer = document.querySelector('.notes');
+
+  //create the list item for the note
+  var listNote = document.createElement('li');
+  listNote.classList += 'list_note';
+  listNote.innerText = title;
+
+  //create the div where the buttons will stay
+  var btnsDiv = document.createElement('div');
+  btnsDiv.classList += 'buttons';
+
+
+  //creating the buttons
+  var editBtn = document.createElement('button'); //create the edit button
+  editBtn.classList += 'edit'; //add the neccesary classes tot he buttons so styling can be applied
+  editBtn.innerHTML = editBtnicon; //append the icons to the buttons
+
+  var deleteBtn = document.createElement('button'); //create the delete button
+  deleteBtn.classList += 'remove'; //add the neccesary classes tot he buttons so styling can be applied
+  deleteBtn.innerHTML = deleteBtnicon; //append the icons to the buttons
+
+  btnsDiv.appendChild(editBtn);
+  btnsDiv.appendChild(deleteBtn);
+  listNote.appendChild(btnsDiv);
+
+  listContainer.insertBefore(listNote, listContainer.childNodes[0]);
+}
